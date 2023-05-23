c92ffa6cc832ec0c79c44a669f3d257f7177d7d5
Gary Griffaw <ggriffaw@yahoo.com>
2023-05-23 14:36:58 -0500

    Correct typo in SpringSecConfig

--- src/main/java/com/garygriffaw/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/com/garygriffaw/spring6restmvc/config/SpringSecConfig.java
@@ -2,7 +2,7 @@ package com.garygriffaw.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-Corimport org.springframework.security.config.Customizer;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
